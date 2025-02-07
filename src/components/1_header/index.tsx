4f88a90727635057e0a0a8ac577cea822f0bac00
Carlos GC <mail@ccebrecos.com>
2025-02-07 17:56:11 +0100

    feat: add header gif

--- src/components/1_header/index.tsx
+++ src/components/1_header/index.tsx
@@ -1,9 +1,13 @@
+import headerGif from '../../assets/img/header.gif';
+
 export default function Header() {
   return (
     <div className="bg-blau-fort instrument-serif-regular text-turquesa flex md:flex-row flex-col space-evenly items-center py-18 md:py-36">
-      <div className="md:w-1/2 flex flex-col md:pl-32 px-10">
-        <div className="text-9xl md:text-[220px] italic text-left">Anna</div>
-        <div className="text-9xl md:text-[220px] mt-0 italic leading-[0.7] text-left">
+      <div className="md:w-1/2 flex flex-col lg:pl-32 pl-10">
+        <div className="text-8xl lg:text-9xl xl:text-[220px] italic text-left">
+          Anna
+        </div>
+        <div className="text-8xl lg:text-9xl xl:text-[220px] mt-0 italic leading-[0.7] text-left">
           & Xus
         </div>
         <div className="poppins-regular text-base mt-9 text-left">
@@ -12,7 +16,9 @@ export default function Header() {
           La Roureda!
         </div>
       </div>
-      <div className="md:w-1/2 text-9xl mt-9 md:mt-0"> FOTO </div>
+      <div className="md:w-1/2 mt-9 md:mt-0 px-10 pb-8 md:pb-0">
+        <img src={headerGif} className="" />
+      </div>
     </div>
   );
 }
