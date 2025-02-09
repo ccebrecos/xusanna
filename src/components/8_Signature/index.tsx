a2289672b6038c00c25c1346a4d520e8b1208cf2
Carlos GC <mail@ccebrecos.com>
2025-02-09 20:20:31 +0100

    Updates

--- src/components/8_Signature/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Signature() {
-  return (
-    <div className="text-xs items-center w-full flex flex-row justify-around text-blau-fort bg-turquesa">
-      <p>{'designed with ♥ by martajace'}</p>
-      <p>{'</> with ♥ by ccebrecos'}</p>
-    </div>
-  );
-}
